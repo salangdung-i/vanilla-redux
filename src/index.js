03068d0ed08e15ae59c849f1249b102b984ecf48
salangdung-i <dladmswl92@naver.com>
2022-04-18 17:45:22 +0900

    주석추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const number = document.querySelector("span");
 let count = 0;
 number.innerText = count;
 
+// html에게 뭔가 바뀌었다고 알려주기 위해 함수를 쓴다는 것 자체가 리덕스가 멋진 이유중 하나이다. 
+// handleAdd, handleMinus를 쓸 때, updateText를 쓰는 이유는 html에 변화를 일으켜야하기 떄문이다. 
 const updateText = ()=> {
   number.innerText = count;
 }
